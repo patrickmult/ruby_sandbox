70516943dfbfe676e9f2b0e3800eb821346e65a1
softcactus <pjmulthaup@gmail.com>
2015-05-28 21:02:18 -0700

    ceaser chipher added

---
+++ ceaser_cipher.rb
@@ -0,0 +1,36 @@
+class Ceaser
+  def initialize(shiftvalue,alphabet = ('a'..'z').to_a.join)
+    realshift = shiftvalue % alphabet.size
+    @encrypt = alphabet[realshift..-1]+alphabet[0...realshift]
+    @decrypt = alphabet
+  end
+
+  def encrypt(string)
+    string.tr(@decrypt, @encrypt)
+  end
+
+  def decrypt(string)
+    string.tr(@encrypt, @decrypt)
+  end
+end
+
+puts "What string would you like encrypted or decrypted?"
+string = gets.chomp
+
+puts "How much would you like it shifted by?"
+shift = gets.chomp.to_i
+
+machine = Ceaser.new(shift)
+
+puts "Would you like it decrypted or encrypted?"
+todo = gets.chomp
+
+if (todo=="encrypted"||todo=="encrypt")
+  puts machine.encrypt(string)  
+else if todo=="decrypted"||"decrypt"
+  puts "test dec"
+else
+  puts "Please say decrypt or encrypt."
+end
+
+end
