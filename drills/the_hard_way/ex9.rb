8469608ec870d84223454803a35a2290a988bd43
softcactus <pjmulthaup@gmail.com>
2015-05-30 14:31:28 -0700

    started 'learn ruby the hard way' drills

---
+++ drills/the_hard_way/ex9.rb
@@ -0,0 +1,12 @@
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+puts "Here are the days: #{days}"
+puts "Here are the months: #{months}"
+
+puts """
+Inside three double quotes
+We type as many new lines as we like.
+Line 3
+"""
+
